15f6552f0840c6a0b449dfdf3d7f5c1325aeb6bc
Yash-Kamtekar <yash.kamtekar@sjsu.edu>
2022-06-12 21:02:51 -0700

    cors error handled

--- CMPE202/main.py
+++ CMPE202/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import database
 from .users import models as user_model
 from .users import main as user_main
@@ -11,5 +12,18 @@ hotel_model.Base.metadata.create_all(bind=database.engine)
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 app.include_router(hotel_main.router)
 app.include_router(user_main.router)
